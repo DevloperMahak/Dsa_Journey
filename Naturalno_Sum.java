a8f4d8abb04c05477e11d0ed343df26392f85aaf
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-06-24 11:17:42 +0530

    Initial commit

---
+++ Naturalno_Sum.java
@@ -0,0 +1,28 @@
+package Logic_Building;
+
+import java.util.Scanner;
+
+class Naturalno_Sum{
+
+    static int findSum(int n) {
+        int sum = 0;
+        // iterating over all the numbers
+        // between 1 to n
+        for (int x = 1; x <= n; x++)
+            sum = sum + x;
+            return sum;
+    }
+
+    static int findTotal(int n)
+    {
+        // Using mathematical formula to compute
+        // sum of first n natural numbers
+        return n * (n + 1) / 2;
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the natural number till which you have to find the sum : ");
+     Scanner input = new Scanner(System.in);
+     int n = input.nextInt();
+        System.out.println("Sum of "+ n + " natural number is :" + findTotal(n));
+    }
+}
